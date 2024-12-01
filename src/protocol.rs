0eab20a9bbdc15c9160d362b45411d6b0c58bd4d
Christoph Sax <c_sax@mailbox.org>
2024-12-01 22:43:02 +0100

    Work on protocol support

--- src/protocol.rs
+++ src/protocol.rs
@@ -2,10 +2,25 @@
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-use serde::{Deserialize};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 type Uri = String;
+type DocumentSelector = Vec<DocumentFilter>;
+
+pub enum ErrorCodes {
+    ParseError = -32700,
+    InvalidRequest = -32600,
+    MethodNotFound = -32601,
+    InvalidParams = -32602,
+    InternalError = -32603,
+    ServerNotInitialized = -32002,
+    UnknownErrorCode = -32001,
+    RequestFailed = -32803,
+    ServerCancelled = -32802,
+    ContentModified = -32801,
+    RequestCancelled = -32800,
+}
 
 #[derive(Deserialize)]
 #[serde(rename_all = "lowercase")]
@@ -172,18 +187,201 @@ pub enum Id {
     Null,
 }
 
-pub enum ErrorCodes {
-    ParseError = -32700,
-    InvalidRequest = -32600,
-    MethodNotFound = -32601,
-    InvalidParams = -32602,
-    InternalError = -32603,
-    ServerNotInitialized = -32002,
-    UnknownErrorCode = -32001,
-    RequestFailed = -32803,
-    ServerCancelled = -32802,
-    ContentModified = -32801,
-    RequestCancelled = -32800,
+pub enum TextDocumentSyncServerCapabilities {
+    TextDocumentSyncOptions,
+    TextDocumentSyncKind,
+}
+
+pub enum TextDocumentSyncKind {
+    None = 0,
+    Full = 1,
+    Incremental = 2,
+}
+
+pub enum NotebookDocumentSyncServerCapabilities {
+    NotebookDocumentSyncOptions,
+    NotebookDocumentSyncRegistrationOptions,
+}
+
+pub enum NotebookSelector {
+    NotebookSelectorByNotebook,
+    NotebookSelectorByCell,
+}
+
+pub enum NotebookSelectorNotebook {
+    String,
+    NotebookDocumentFilter,
+}
+
+pub enum NotebookDocumentFilter {
+    NotebookDocumentFilterByType,
+    NotebookDocumentFilterByScheme,
+    NotebookDocumentFilterByPattern,
+}
+
+pub enum GlobPattern {
+    String,
+    RelativePattern,
+}
+
+pub enum HoverProvider {
+    Bool(bool),
+    HoverOptions,
+}
+
+pub enum DeclarationProvider {
+    Bool(bool),
+    DeclarationOptions,
+    DeclarationRegistrationOptions,
+}
+
+pub enum DefinitionProvider {
+    Bool(bool),
+    DefinitionOptions,
+}
+
+pub enum TypeDefinitionProvider {
+    Bool(bool),
+    TypeDefinitionOptions,
+    TypeDefinitionRegistrationOptions,
+}
+
+pub enum ImplementationProvider {
+    Bool(bool),
+    ImplementationOptions,
+    ImplementationRegistrationOptions,
+}
+
+pub enum ReferencesProvider {
+    Bool(bool),
+    ReferenceOptions,
+}
+
+pub enum DocumentHighlightProvider {
+    Bool(bool),
+    DocumentHighlightOptions,
+}
+
+pub enum DocumentSymbolProvider {
+    Bool(bool),
+    DocumentSymbolOptions,
+}
+
+pub enum CodeActionProvider {
+    Bool(bool),
+    CodeActionOptions,
+}
+
+pub enum ColorProvider {
+    Bool(bool),
+    DocumentColorOptions,
+    DocumentColorRegistrationOptions,
+}
+
+pub enum DocumentFormattingProvider {
+    Bool(bool),
+    DocumentFormattingOptions,
+}
+
+pub enum DocumentRangeFormattingProvider {
+    Bool(bool),
+    DocumentRangeFormattingOptions,
+}
+
+pub enum RenameProvider {
+    Bool(bool),
+    RenameOptions,
+}
+
+pub enum FoldingRangeProvider {
+    Bool(bool),
+    FoldingRangeOptions,
+    FoldingRangeRegistrationOptions,
+}
+
+pub enum SelectionRangeProvider {
+    Bool(bool),
+    SelectionRangeOptions,
+    SelectionRangeRegistrationOptions,
+}
+
+pub enum LinkedEditingRangeProvider {
+    Bool(bool),
+    LinkedEditingRangeOptions,
+    LinkedEditingRangeRegistrationOptions,
+}
+
+pub enum CallHierarchyProvider {
+    Bool(bool),
+    CallHierarchyOptions,
+    CallHierarchyRegistrationOptions,
+}
+
+pub enum SemanticTokensProvider {
+    SemanticTokensOptions,
+    SemanticTokensRegistrationOptions,
+}
+
+pub enum SemanticTokensFullDocumentCapabilities {
+    Bool(bool),
+    Delta { delta: Option<bool> },
+}
+
+pub enum MonikerProvider {
+    Bool(bool),
+    MonikerOptions,
+    MonikerRegistrationOptions,
+}
+
+pub enum TypeHierarchyProvider {
+    Bool(bool),
+    TypeHierarchyOptions,
+    TypeHierarchyRegistrationOptions,
+}
+
+pub enum InlineValueProvider {
+    Bool(bool),
+    InlineValueOptions,
+    InlineValueRegistrationOptions,
+}
+
+pub enum InlayHintProvider {
+    Bool(bool),
+    InlayHintOptions,
+    InlayHintRegistrationOptions,
+}
+
+pub enum DiagnosticProvider {
+    DiagnosticOptions,
+    DiagnosticRegistrationOptions,
+}
+
+pub enum WorkspaceSymbolProvider {
+    Bool(bool),
+    WorkspaceSymbolOptions,
+}
+
+pub enum InlineCompletionProvider {
+    Bool(bool),
+    InlineCompletionOptions,
+}
+
+pub enum ChangeNotifications {
+    String,
+    Bool(bool),
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum FileOperationPatternKind {
+    File,
+    Folder,
+}
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum InitializeErrorCodes {
+    unknownProtocolVersion = 1,
 }
 
 pub struct RequestMessage {
@@ -200,16 +398,17 @@ pub struct ResponseMessage {
     error: Option<ResponseError>,
 }
 
+pub struct NotificationMessage {
+    jsonrpc: String,
+    method: String,
+    params: Value,
+}
+
 #[derive(Debug)]
 pub struct ResponseError {
     pub code: i64,
     pub message: String,
     pub data: Option<Value>,
-}
-
-pub struct NotificationMessage {
-    jsonrpc: String,
-    method: String,
 }
 
 pub struct InitializeParams {
@@ -690,3 +889,421 @@ pub struct ProgressToken {
     token: ProgressTokenKind,
     value: Value,
 }
+
+pub struct InitializeResult {
+    capabilities: ServerCapabilities,
+    server_info: Option<ServerInfo>,
+}
+
+pub struct InitializeError {
+    retry: bool,
+}
+
+pub struct ServerCapabilities {
+    position_encoding: Option<PositionEncodingKind>,
+    text_document_sync: Option<TextDocumentSyncServerCapabilities>,
+    notebook_document_sync: Option<NotebookDocumentSyncServerCapabilities>,
+    completion_provider: Option<CompletionOptions>,
+    hover_provider: Option<HoverProvider>,
+    signature_help_provider: Option<SignatureHelpOptions>,
+    declaration_provider: Option<DeclarationProvider>,
+    definition_provider: Option<DefinitionOptions>,
+    type_definition_provider: Option<TypeDefinitionProvider>,
+    implementation_provider: Option<ImplementationProvider>,
+    references_provider: Option<ReferencesProvider>,
+    document_highlight_provider: Option<DocumentHighlightProvider>,
+    document_symbol_provider: Option<DocumentSymbolProvider>,
+    code_action_provider: Option<CodeActionOptions>,
+    code_lens_provider: Option<CodeLensOptions>,
+    document_link_provider: Option<DocumentLinkOptions>,
+    color_provider: Option<ColorProvider>,
+    document_formatting_provider: Option<DocumentFormattingOptions>,
+    document_range_formatting_provider: Option<DocumentRangeFormattingProvider>,
+    document_on_type_formatting_provider: Option<DocumentOnTypeFormattingOptions>,
+    rename_provider: Option<RenameProvider>,
+    folding_range_provider: Option<FoldingRangeProvider>,
+    execute_command_provider: Option<ExecuteCommandOptions>,
+    selection_range_provider: Option<SelectionRangeProvider>,
+    linked_editing_range_provider: Option<LinkedEditingRangeProvider>,
+    call_hierarchy_provider: Option<CallHierarchyProvider>,
+    semantic_tokens_provider: Option<SemanticTokensProvider>,
+    moniker_provider: Option<MonikerProvider>,
+    type_hierarchy_provider: Option<TypeHierarchyProvider>,
+    inline_value_provider: Option<InlineValueProvider>,
+    inlay_hint_provider: Option<InlayHintProvider>,
+    diagnostic_provider: Option<DiagnosticProvider>,
+    workspace_symbol_provider: Option<WorkspaceSymbolProvider>,
+    inline_completion_provider: Option<InlineCompletionProvider>,
+    text_document: Option<TextDocumentServerCapabilities>,
+    workspace: Option<WorkspaceServerCapabilities>,
+    experimental: Option<Value>,
+}
+
+pub struct ServerInfo {
+    name: String,
+    version: Option<String>,
+}
+
+pub struct TextDocumentSyncOptions {
+    open_close: Option<bool>,
+    change: Option<TextDocumentSyncKind>,
+}
+
+pub struct NotebookDocumentSyncOptions {
+    notebook_selector: Vec<NotebookSelector>,
+    save: Option<bool>,
+}
+
+pub struct NotebookDocumentSyncRegistrationOptions {
+    notebook_selector: Vec<NotebookSelector>,
+    save: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct NotebookSelectorByNotebook {
+    notebook: NotebookSelectorNotebook,
+    cells: Option<Vec<NotebookSelectorCell>>,
+}
+
+pub struct NotebookSelectorByCell {
+    notebook: Option<NotebookSelectorNotebook>,
+    cells: Vec<NotebookSelectorCell>,
+}
+
+pub struct NotebookDocumentFilterByType {
+    notebook_type: String,
+    scheme: Option<String>,
+    pattern: Option<GlobPattern>,
+}
+
+pub struct NotebookDocumentFilterByScheme {
+    notebook_type: Option<String>,
+    scheme: String,
+    pattern: Option<GlobPattern>,
+}
+
+pub struct NotebookDocumentFilterByPattern {
+    notebook_type: Option<String>,
+    scheme: Option<String>,
+    pattern: GlobPattern,
+}
+
+pub struct RelativePattern {
+    base_uri: WorkspaceFolder,
+    pattern: String,
+}
+
+pub struct NotebookSelectorCell {
+    language: String,
+}
+
+pub struct CompletionOptions {
+    trigger_characters: Option<Vec<String>>,
+    all_commit_characters: Option<Vec<String>>,
+    resolve_provider: Option<bool>,
+    completion_item: Option<CompletionOptionsCompletionItemCapability>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct CompletionOptionsCompletionItemCapability {
+    label_details_support: Option<bool>,
+}
+
+pub struct HoverOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct SignatureHelpOptions {
+    trigger_characters: Vec<String>,
+    retrigger_characters: Vec<String>,
+}
+
+pub struct DeclarationOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct DeclarationRegistrationOptions {
+    work_done_progress: Option<bool>,
+    document_selector: Option<DocumentSelector>,
+    id: Option<String>,
+}
+
+pub struct DocumentFilter {
+    language: Option<String>,
+    scheme: Option<String>,
+    pattern: Option<GlobPattern>,
+}
+
+pub struct DefinitionOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct TypeDefinitionOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct TypeDefinitionRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct ImplementationOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct ImplementationRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct ReferenceOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentHighlightOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentSymbolOptions {
+    label: Option<String>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct CodeActionOptions {
+    code_action_kinds: Option<Vec<CodeActionKind>>,
+    documentation: Option<Vec<CodeActionKindDocumentation>>,
+    resolve_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct CodeActionKindDocumentation {
+    kind: CodeActionKind,
+    command: Command,
+}
+
+pub struct Command {
+    title: String,
+    tooltip: Option<String>,
+    command: String,
+    arguments: Option<Vec<Value>>,
+}
+
+pub struct CodeLensOptions {
+    resolve_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentLinkOptions {
+    resolve_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentColorOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentColorRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    id: Option<String>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentFormattingOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentRangeFormattingOptions {
+    ranges_support: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct DocumentOnTypeFormattingOptions {
+    first_trigger_character: String,
+    more_trigger_character: Option<Vec<String>>,
+}
+
+pub struct RenameOptions {
+    prepare_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct FoldingRangeOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct FoldingRangeRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct ExecuteCommandOptions {
+    commands: Vec<String>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct SelectionRangeOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct SelectionRangeRegistrationOptions {
+    work_done_progress: Option<bool>,
+    document_selector: Option<DocumentSelector>,
+    id: Option<String>,
+}
+
+pub struct LinkedEditingRangeOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct LinkedEditingRangeRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct CallHierarchyOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct CallHierarchyRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct SemanticTokensOptions {
+    legend: SemanticTokensLegend,
+    range: Option<bool>,
+    full: Option<SemanticTokensFullDocumentCapabilities>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct SemanticTokensLegend {
+    token_types: Vec<String>,
+    token_modifiers: Vec<String>,
+}
+
+pub struct SemanticTokensRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    legend: SemanticTokensLegend,
+    range: Option<bool>,
+    full: Option<SemanticTokensFullDocumentCapabilities>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct MonikerOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct MonikerRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct TypeHierarchyOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct TypeHierarchyRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct InlineValueOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct InlineValueRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct InlayHintOptions {
+    resolve_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct InlayHintRegistrationOptions {
+    resolve_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+    document_selector: Option<DocumentSelector>,
+    id: Option<String>,
+}
+
+pub struct DiagnosticOptions {
+    identifier: Option<String>,
+    inter_file_dependencies: bool,
+    workspace_diagnostics: bool,
+    work_done_progress: Option<bool>,
+}
+
+pub struct DiagnosticRegistrationOptions {
+    document_selector: Option<DocumentSelector>,
+    identifier: Option<String>,
+    inter_file_dependencies: bool,
+    workspace_diagnostics: bool,
+    work_done_progress: Option<bool>,
+    id: Option<String>,
+}
+
+pub struct WorkspaceSymbolOptions {
+    resolve_provider: Option<bool>,
+    work_done_progress: Option<bool>,
+}
+
+pub struct InlineCompletionOptions {
+    work_done_progress: Option<bool>,
+}
+
+pub struct TextDocumentServerCapabilities {
+    diagnostic: Option<TextDocumentDiagnosticServerCapabilities>,
+}
+
+pub struct TextDocumentDiagnosticServerCapabilities {
+    markup_message_support: Option<bool>,
+}
+
+pub struct WorkspaceServerCapabilities {
+    workspace_folders: Option<WorkspaceFoldersServerCapabilities>,
+}
+
+pub struct WorkspaceFoldersServerCapabilities {
+    supported: Option<bool>,
+    change_notifications: Option<ChangeNotifications>,
+    file_operations: Option<WorkspaceFileOperations>,
+}
+
+pub struct WorkspaceFileOperations {
+    did_create: Option<FileOperationRegistrationOptions>,
+    will_create: Option<FileOperationRegistrationOptions>,
+    did_rename: Option<FileOperationRegistrationOptions>,
+    will_rename: Option<FileOperationRegistrationOptions>,
+    did_delete: Option<FileOperationRegistrationOptions>,
+    will_delete: Option<FileOperationRegistrationOptions>,
+}
+
+pub struct FileOperationRegistrationOptions {
+    filters: Vec<FileOperationFilter>,
+}
+
+pub struct FileOperationFilter {
+    scheme: Option<String>,
+    pattern: FileOperationPattern,
+}
+
+pub struct FileOperationPattern {
+    glob: String,
+    matches: Option<FileOperationPatternKind>,
+    options: Option<FileOperationPatternOptions>,
+}
+
+pub struct FileOperationPatternOptions {
+    ignore_case: Option<bool>,
+}
+
