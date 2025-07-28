f1e1f87f35d43fbcbcb569706e5a9b780d4f55e9
Christoph Sax <c_sax@mailbox.org>
2025-07-28 19:57:16 +0200

    Add missing license header

--- ls/src/utils.rs
+++ ls/src/utils.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Christoph Sax <c_sax@mailbox.org>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use std::{convert::From, ops::Range};
 
 use tree_sitter::Range as TRange;
